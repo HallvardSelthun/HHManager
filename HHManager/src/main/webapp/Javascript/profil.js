43196595aa2f35bbc07010b16c2e23e91e4f1907
noraor <noraor@stud.ntnu.no>
2018-01-18 10:09:25 +0100

    Forlat husstand knapp fin

--- HHManager/src/main/webapp/Javascript/profil.js
+++ HHManager/src/main/webapp/Javascript/profil.js
@@ -384,7 +384,7 @@ function hentliste() {
         $("#husstander").append('<div class="panel panel-default"><div class="panel-heading clearfix" data-toggle="collapse" data-parent="#husstander"' +
             ' data-target="#' + husholdningId + '" onclick="displayDiv()"><h4 id="tittel" class="panel-title col-md-9"><a></a>' + husholdnavn + '</h4>' +
             '<div><button id="meldut"' +
-            ' class="btn btn-danger pull-right removeButton col-md-3" type="button">Forlat</button></div>' +
+            ' class="btn btn-danger pull-right removeButton col-md-3" type="button" style="width: 70px">Forlat</button></div>' +
             '</div><div id="' + husholdningId + '"' +
             ' class="panel-collapse collapse invisibleDiv"><div class="panel-body"><ul class="list-group"></ul>' +
             '<div id="list1" class="list-group">' + '</div></div></div></div>');
