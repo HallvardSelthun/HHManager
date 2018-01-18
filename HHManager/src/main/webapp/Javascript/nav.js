ff9d054d6a2a559dc78cd847d18d86d41ccba49b
Brogztato <bragehs@hotmail.com>
2018-01-18 11:08:26 +0100

    navbar husholdninger

--- HHManager/src/main/webapp/Javascript/nav.js
+++ HHManager/src/main/webapp/Javascript/nav.js
@@ -22,97 +22,99 @@ $(document).ready(function () {
             console.log(i + "_---_");
 
         });
-
-        $('body').on('click', 'a#bildenav', function () {
-            window.location = "forside.html"
-        });
-        $('body').on('click', 'a#gjoremaalsknapp', function () {
-            window.location = "gjoremaal.html"
-        });
-        $('body').on('click', 'a#kalenderknapp', function () {
-            window.location = "kalender.html"
-        });
-        $('body').on('click', 'a#handlelisteknapp', function () {
-            window.location = "handlelister.html"
-        });
-        $('body').on('click', 'a#bildeknapp', function () {
-            window.location = "forside.html"
-        });
-
-        $('body').on('click', 'a#profilNavn', function () {
-            window.location = "profil.html"
-        });
-
-        $('body').on('click', '#oppgjorknapp', function () {
-            window.location = "oppgjor.html"
-        });
-
-        $('body').on('click', 'a#loggut', function () {
-            localStorage.clear();
-            window.location = "index.html"
-        });
-
-        // til lagNyHusstandModalen
-        $('body').on('click', '#leggTilMedlemKnapp', function () {
-            var medlem = {
-                epost: $("#navnMedlem").val()
-            };
-            $("#navnMedlem").val("");
-            navnIHuset.push(medlem);
-            console.log(navnIHuset);
-            $("#fade").show();
-            console.log("funker");
-        });
-
-        $('body').on('click', "a#alertbox", function () {
-            $("#fade").hide();
-            console.log("hide");
-        });
-
-        $("body").on("click", "#lagreHusKnapp", function () {
-            var navnHus = $("#navnHusstand").val();
-            var medlemHus = $("#navnMedlem").val();
-
-            var husObj = {
-                navn: navnHus,
-                medlemmer: navnIHuset,
-                adminId: 1 // denne verdien er ikke konstant. Bare for testing til ting er på plass
-            };
-            console.log(husObj);
-            console.log("Prøver å sende husstand");
-
-            if (navnHus == "" || medlemHus == "") {
-                alert("Skriv inn noe");
-                return;
-                window.location = "index.html"
-                console.log("inni if");
-            }
-            $.ajax({
-                url: "server/hhservice/husholdning",
-                type: 'POST',
-                data: JSON.stringify(husObj),
-                contentType: 'application/json; charset=utf-8',
-                dataType: 'json',
-                success: function (result) {
-                    var data = JSON.parse(result); // gjør string til json-objekt
-                    console.log("Data: " + data);
-                    if (data) {
-                        alert("Det gikk bra!");
-                    } else {
-                        alert("feil!");
-                    }
-                },
-                error: function () {
-                    alert("serverfeil :/");
-                    console.log(husObj)
-                }
-            });
-            setTimeout(function () {
-                $("a#profilNavn").text(navn);
-            }, 150);
-
-        });
     });
+
+    $('body').on('click', 'a#bildenav', function () {
+        window.location = "forside.html"
+    });
+    $('body').on('click', 'a#gjoremaalsknapp', function () {
+        window.location = "gjoremaal.html"
+    });
+    $('body').on('click', 'a#kalenderknapp', function () {
+        window.location = "kalender.html"
+    });
+    $('body').on('click', 'a#handlelisteknapp', function () {
+        window.location = "handlelister.html"
+    });
+    $('body').on('click', 'a#bildeknapp', function () {
+        window.location = "forside.html"
+    });
+
+    $('body').on('click', 'a#profilNavn', function () {
+        window.location = "profil.html"
+    });
+
+    $('body').on('click', '#oppgjorknapp', function () {
+        window.location = "oppgjor.html"
+    });
+
+    $('body').on('click', 'a#loggut', function () {
+        localStorage.clear();
+        window.location = "index.html"
+    });
+
+    // til lagNyHusstandModalen
+    $('body').on('click', '#leggTilMedlemKnapp', function () {
+        var medlem = {
+            epost: $("#navnMedlem").val()
+        };
+        $("#navnMedlem").val("");
+        navnIHuset.push(medlem);
+        console.log(navnIHuset);
+        $("#fade").show();
+        console.log("funker");
+    });
+
+    $('body').on('click', "a#alertbox", function () {
+        $("#fade").hide();
+        console.log("hide");
+    });
+
+    $("body").on("click", "#lagreHusKnapp", function () {
+        var navnHus = $("#navnHusstand").val();
+        var medlemHus = $("#navnMedlem").val();
+
+        var husObj = {
+            navn: navnHus,
+            medlemmer: navnIHuset,
+            adminId: 1 // denne verdien er ikke konstant. Bare for testing til ting er på plass
+        };
+        console.log(husObj);
+        console.log("Prøver å sende husstand");
+
+        if (navnHus == "" || medlemHus == "") {
+            alert("Skriv inn noe");
+            return;
+            window.location = "index.html"
+            console.log("inni if");
+        }
+        $.ajax({
+            url: "server/hhservice/husholdning",
+            type: 'POST',
+            data: JSON.stringify(husObj),
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json',
+            success: function (result) {
+                var data = JSON.parse(result); // gjør string til json-objekt
+                console.log("Data: " + data);
+                if (data) {
+                    alert("Det gikk bra!");
+                } else {
+                    alert("feil!");
+                }
+            },
+            error: function () {
+                alert("serverfeil :/");
+                console.log(husObj)
+            }
+        });
+        setTimeout(function () {
+            $("a#profilNavn").text(navn);
+        }, 150);
+
+    });
+
+
 });
 
 
@@ -129,4 +131,5 @@ function getHusholdninger() {
         }
     }, 250);
 }
+
 
