14f2dfa3cf169c8accb9c3286ef3f223efe18500
tonivuc <tonivuc@gmail.com>
2018-01-19 10:36:48 +0100

    work in progress

--- src/main/webapp/Javascript/oppgjor.js
+++ src/main/webapp/Javascript/oppgjor.js
@@ -1,3 +1,26 @@
 $(document).ready(function () {
 
+    function
+
+    $.ajax({
+        url: "server/BrukerService/registrer",
+        type: 'POST',
+        data: JSON.stringify(bruker),
+        contentType: 'application/json; charset=utf-8',
+        dataType: 'json',
+        success: function (result) {
+            var data = JSON.parse(result);
+            console.log(data +" :D");
+            if (!result){
+                alert("Epost er allerede registrert :/");
+                $("#email").css('color', 'red');
+            }else{
+                alert("Bruker registrert!");
+                window.location = "index.html";
+            }
+        },
+        error: function () {
+            alert("Serveren har det røft atm, prøv igjen senere :/");
+        }
+    })
 });
