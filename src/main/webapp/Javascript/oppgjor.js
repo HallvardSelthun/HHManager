e4b0348fad972fb1bb0f857d50495880e308ba22
tonivuc <tonivuc@gmail.com>
2018-01-19 14:37:38 +0100

    Fungerende test av jQuery Templates for å sette inn HTML-kode dynamisk

--- src/main/webapp/Javascript/oppgjor.js
+++ src/main/webapp/Javascript/oppgjor.js
@@ -1,28 +1,35 @@
 $(document).ready(function () {
 
     function lastInnOppgjor() {
-
+        $.ajax({
+            url: "server/BrukerService/registrer",
+            type: 'POST',
+            data: JSON.stringify(bruker),
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json',
+            success: function (result) {
+                var data = JSON.parse(result);
+                console.log(data +" :D");
+                if (!result){
+                    alert("Epost er allerede registrert :/");
+                    $("#email").css('color', 'red');
+                }else{
+                    alert("Bruker registrert!");
+                    window.location = "index.html";
+                }
+            },
+            error: function () {
+                alert("Serveren har det røft atm, prøv igjen senere :/");
+            }
+        })
     }
 
-    $.ajax({
-        url: "server/BrukerService/registrer",
-        type: 'POST',
-        data: JSON.stringify(bruker),
-        contentType: 'application/json; charset=utf-8',
-        dataType: 'json',
-        success: function (result) {
-            var data = JSON.parse(result);
-            console.log(data +" :D");
-            if (!result){
-                alert("Epost er allerede registrert :/");
-                $("#email").css('color', 'red');
-            }else{
-                alert("Bruker registrert!");
-                window.location = "index.html";
-            }
-        },
-        error: function () {
-            alert("Serveren har det røft atm, prøv igjen senere :/");
-        }
-    })
+    var template = $('#hidden-template').html();
+
+    $("#buttonnn").click(function() {
+        $('#right').append(template);
+    });
+
+
+
 });
