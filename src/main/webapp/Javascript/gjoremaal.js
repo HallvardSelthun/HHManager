d6e4515500a906637da34e7f2ecbc1b4ca9f70ba
Charlotte <charlotte.groder@gmail.com>
2018-01-19 13:14:05 +0100

    bytter branch må pushe

--- src/main/webapp/Javascript/gjoremaal.js
+++ src/main/webapp/Javascript/gjoremaal.js
@@ -1,4 +1,16 @@
+var husholdning;
+var bruker = JSON.parse(localStorage.getItem("bruker"));
+var epost = bruker.epost;
+var brukerId;
+var husholdningId
+var medlemmer;
+
 $(document).ready(function () {
-    $('#leggtil').click(function ()  {
-    })
+    husholdningId = bruker.favHusholdning;
+
+    gethhData();
+    setTimeout(setupPage,1000);
+    $("#leggtil").on("click", function () {
+        postInnlegg();
+    });
 });
