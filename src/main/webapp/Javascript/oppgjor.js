10367924d6bda47c7ea8029fe4d0ef6168c31ea1
tonivuc <tonivuc@gmail.com>
2018-01-23 11:06:50 +0100

    Ryddet litt i oppgjor.js-koden

--- src/main/webapp/Javascript/oppgjor.js
+++ src/main/webapp/Javascript/oppgjor.js
@@ -7,135 +7,102 @@ $(document).ready(function () {
         lastinn()
     },250);
 
-    //Globale variabler
-    var testBrukerId = 1;
-    var alleOppgjor = [];
-
     //Kjør JavaScript
     init();
 
-    function init() {
-        lastInnOppgjor(testBrukerId);
-        //Resten av funksjonene ligger i callbacks for å sørge for riktig rekkefølge.
-    }
-
-    function utregnOppgjorSum() {
-
-        var sum = 0;
-        var totalSum = 0;
-        console.log("alleOppgjor.length burde være 3 men er: "+alleOppgjor.length);
-        for (var i = 0; i < alleOppgjor.length; i++) {
-            console.log("Nå har vi kjørt loopen "+i+" ganger.");
-            for (var j = 0; j < alleOppgjor[i].utleggJegSkylder.length; j++) {
-                sum = sum - alleOppgjor[i].utleggJegSkylder[j].delSum;
-            }
-            alleOppgjor[i].skylderSum = sum;
-            totalSum = sum;
-            sum = 0;
-            console.log("alleOppgjor: "+i);
-            console.log(alleOppgjor[i])
-
-            var length = alleOppgjor[i].utleggDenneSkylderMeg.length;
-            console.log(length)
-
-            for (j = 0; j < length; j++) {
-                sum = sum + alleOppgjor[i].utleggDenneSkylderMeg[j].delSum;
-            }
-
-            alleOppgjor[i].skylderMegSum = sum;
-            totalSum = totalSum + sum;
-            alleOppgjor[i].totalSum = totalSum;
-        }
-
-        displayOppgjor();
-    }
-
-    function displayOppgjor() {
-
-        // Compile the markup as a named template
-        $.template( "oppgjorTemplate", $("#test-oppgjor"));
-
-        $.template("rad-template", $("#rad-template"));
-        //Append compiled markup
-
-        for (var i = 0; i < alleOppgjor.length; i++) {
-            $.tmpl( "oppgjorTemplate", alleOppgjor[i]).appendTo($("#panelGruppe"));
-
-            $.tmpl( "rad-template", alleOppgjor[i].utleggJegSkylder).appendTo($("#radMinus"+i+""));
-            $.tmpl( "rad-template", alleOppgjor[i].utleggDenneSkylderMeg).appendTo($("#radPlus"+i+""));
-        }
-    }
-
-    //Legg til indekser på rader og oppgjør så de er raskere å finne senere
-    function leggInnRadNr(callback) {
-        for (var i = 0; i < alleOppgjor.length; i++) {
-            alleOppgjor[i].oppgjorNr = i;
-            for (var j = 0; j < alleOppgjor[i].utleggJegSkylder.length; j++) {
-                alleOppgjor[i].utleggJegSkylder.radNr = j;
-            }
-
-            for (var j = 0; j < alleOppgjor[i].utleggDenneSkylderMeg.length; j++) {
-                alleOppgjor[i].utleggDenneSkylderMeg.radNr = j;
-            }
-        }
-        callback();
-    }
-
-
-
-
-    //SQL-kall
-    function lastInnOppgjor(brukerId) {
-        $.ajax({
-            url: "server/utlegg/oppgjor/"+ brukerId,
-            type: 'GET',
-            contentType: 'application/json; charset=utf-8',
-            dataType: 'json',
-            success: function (result) {
-                alleOppgjor = result;
-                if (!result){
-                    alert("Noe rart har skjedd i lastInnOppgjor");
-                }else{
-                    console.log(result);
-                    leggInnRadNr(utregnOppgjorSum);
-                }
-            },
-            error: function () {
-                alert("Serveren har det røft atm, prøv igjen senere :/");
-            }
-        })
-    }
-
-    var rad = {
-        sum: -36,
-        beskrivelse: "Pepperkakedeig",
-        betalt: false,
-        radNr: -1
-    };
-
-    var skylderRad = {
-        sum: -36,
-        beskrivelse: "Pepperkakedeig",
-        betalt: false,
-        radNr: -1
-    };
-
-    var skylderRad2 = {
-        sum: -60,
-        beskrivelse: "Taxi hjem",
-        betalt: false,
-        radNr: 2
-    };
-
-    var oppgjor = {
-        utleggJegSkylder: [],
-        utleggDenneSkylderMeg: [],
-        brukerId: 123,
-        navn: "Toni Vucic",
-        oppgjorNr: -1 //Denne burde genereres når objektet kommer inn i JavaScript
-    };
-
 });
+
+//Globale variabler
+var testBrukerId = 1;
+var alleOppgjor = [];
+
+function init() {
+    lastInnOppgjor(testBrukerId);
+    //Resten av funksjonene ligger i callbacks for å sørge for riktig rekkefølge.
+}
+
+function utregnOppgjorSum() {
+
+    var sum = 0;
+    var totalSum = 0;
+    for (var i = 0; i < alleOppgjor.length; i++) {
+        for (var j = 0; j < alleOppgjor[i].utleggJegSkylder.length; j++) {
+            sum = sum - alleOppgjor[i].utleggJegSkylder[j].delSum;
+        }
+        alleOppgjor[i].skylderSum = sum;
+        totalSum = sum;
+        sum = 0;
+        for (j = 0; j < alleOppgjor[i].utleggDenneSkylderMeg.length; j++) {
+            sum = sum + alleOppgjor[i].utleggDenneSkylderMeg[j].delSum;
+        }
+        alleOppgjor[i].skylderMegSum = sum;
+        totalSum = totalSum + sum;
+        if (totalSum > 0) {
+            alleOppgjor[i].posNeg = "Pos";
+        }
+        else {
+            alleOppgjor[i].posNeg = "Neg";
+        }
+        alleOppgjor[i].totalSum = totalSum;
+    }
+
+    displayOppgjor();
+}
+
+function merk
+
+function displayOppgjor() {
+
+    // Compile the markup as a named template
+    $.template( "oppgjorTemplate", $("#test-oppgjor"));
+
+    $.template("rad-template", $("#rad-template"));
+    //Append compiled markup
+
+    for (var i = 0; i < alleOppgjor.length; i++) {
+        $.tmpl( "oppgjorTemplate", alleOppgjor[i]).appendTo($("#panelGruppe"));
+
+        $.tmpl( "rad-template", alleOppgjor[i].utleggJegSkylder).appendTo($("#radMinus"+i+""));
+        $.tmpl( "rad-template", alleOppgjor[i].utleggDenneSkylderMeg).appendTo($("#radPlus"+i+""));
+    }
+}
+
+//Legg til indekser på rader og oppgjør så de er raskere å finne senere
+function leggInnRadNr(callback) {
+    for (var i = 0; i < alleOppgjor.length; i++) {
+        alleOppgjor[i].oppgjorNr = i;
+        for (var j = 0; j < alleOppgjor[i].utleggJegSkylder.length; j++) {
+            alleOppgjor[i].utleggJegSkylder.radNr = j;
+        }
+
+        for (var j = 0; j < alleOppgjor[i].utleggDenneSkylderMeg.length; j++) {
+            alleOppgjor[i].utleggDenneSkylderMeg.radNr = j;
+        }
+    }
+    callback();
+}
+
+//SQL-kall
+function lastInnOppgjor(brukerId) {
+    $.ajax({
+        url: "server/utlegg/oppgjor/"+ brukerId,
+        type: 'GET',
+        contentType: 'application/json; charset=utf-8',
+        dataType: 'json',
+        success: function (result) {
+            alleOppgjor = result;
+            if (!result){
+                alert("Noe rart har skjedd i lastInnOppgjor");
+            }else{
+                console.log(result);
+                leggInnRadNr(utregnOppgjorSum);
+            }
+        },
+        error: function () {
+            alert("Serveren har det røft atm, prøv igjen senere :/");
+        }
+    })
+}
 
 
 function lagNyttUtlegg() {
@@ -182,6 +149,7 @@ function lagNyttUtlegg() {
         }
     })
 }
+
 function lastinn() {
     var husholdninger = JSON.parse(localStorage.getItem("husholdninger"));
     var husId = localStorage.getItem("husholdningId");
@@ -190,6 +158,7 @@ function lastinn() {
         if (husholdninger[j].husholdningId==husId){
             for(var k =0 , l = husholdninger[j].medlemmer.length; k<l; k++){
                 $.template( "medlemmer", $("#listeMedlem"));
+                console.log(husholdninger[j]);
                 $.tmpl("medlemmer", husholdninger[j].medlemmer[k]).appendTo($("#personer"));
             }
         }
