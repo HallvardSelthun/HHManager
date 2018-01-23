024df058bf6c855ec1837f930ebe95a691ed5c3e
hallvsa <hallvsa@stud.ntnu.no>
2018-01-23 12:44:19 +0100

    Merge?

--- src/main/java/server/controllers/BrukerController.java
+++ src/main/java/server/controllers/BrukerController.java
@@ -21,13 +21,47 @@ public class BrukerController {
         return GenereltController.getString("navn", TABELLNAVN, brukerid);
     }
 
+    /**
+     * Henter epost-adressen til en bruker gitt brukerens id.
+     * @param brukerid int id som identifiserer en bruker.
+     * @return String epost-adressen.
+     */
     public static String getEpost(int brukerid) {
         return GenereltController.getString("epost", TABELLNAVN, brukerid);
     }
 
+    /**
+     * Henter hvilken husholdning som er favoritt gitt en brukers id.
+     * @param brukerid int id som identifiserer en bruker
+     * @return String navnet på husholdningen satt som favoritt.
+     */
     public static String getFavoritthusholdning(int brukerid) {
         return GenereltController.getString("favorittHusholdning", TABELLNAVN, brukerid);
     }
+
+    /**
+     * Sletter et medlem fra en husholdning gitt brukerens id.
+     * @param brukerid int id som identifiserer en bruker
+     * @return true om brukeren ble slettet, false om noe gikk galt under sletting.
+     */
+    public static boolean slettFraHusholdning(int brukerid, int husholdningid) {
+        String getQuery = "DELETE FROM hhmedlem WHERE brukerId = " + brukerid + " AND husholdningId =" + husholdningid;
+
+        try (Connection con = ConnectionPool.getConnection()){
+            ps = con.prepareStatement(getQuery);
+            ps.executeUpdate();
+            return true;
+        }catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    /**
+     * Registrerer en bruker i systemet.
+     * @param bruker er et Bruker-objekt som skal registreres
+     * @return true dersom bruker ble registrert, false om noe gikk galt under registrering.
+     */
 
     public static boolean registrerBruker(Bruker bruker) {
         String pass = bruker.getPassord();
@@ -63,14 +97,14 @@ public class BrukerController {
     }
 
     /**
-     * Sjekker om epost og passord stemmer
-     *
+     * Sjekker om epost og passord stemmer.
      * @param epost
      * @param passord
      * @return true hvis dataene stemmer
      */
     public static Bruker loginOk(String epost, String passord) {
         String query = "SELECT passord, favorittHusholdning, navn, brukerId FROM bruker WHERE epost = ?";
+
         Bruker bruker = new Bruker();
         int favHus = 0;
         bruker.setFavHusholdning(favHus);
@@ -88,6 +122,17 @@ public class BrukerController {
                     bruker.setFavHusholdning(favHusDB);
                 }
                 if (res.equals(passord)) {
+                    String hentGjoremal = "SELECT * FROM gjøremål WHERE utførerId = " + bruker.getBrukerId() + " AND fullført = 0";
+                    ps = con.prepareStatement(hentGjoremal);
+                    ResultSet rs2 = ps.executeQuery();
+                    while(rs2.next()){
+                        Gjøremål gjøremål = new Gjøremål();
+                        gjøremål.setFrist(rs2.getDate("frist"));
+                        gjøremål.setBeskrivelse(rs2.getString("beskrivelse"));
+                        gjøremål.setGjøremålId(rs2.getInt("gjøremålId"));
+                        gjøremål.setHhBrukerId(bruker.getBrukerId());
+                        bruker.addGjøremål(gjøremål);
+                    }
                     return bruker;
                 }
             }
@@ -103,7 +148,7 @@ public class BrukerController {
      * @return true hvis operasjonen ble godkjent
      */
     public static void setNyFavoritthusholdning(int brukerId, String husholdningId) {
-        GenereltController.update(TABELLNAVN, "husholdningId", husholdningId, brukerId);
+         GenereltController.update(TABELLNAVN, "husholdningId", husholdningId, brukerId);
     }
 
     public static Bruker getBrukerData(String epost) {
