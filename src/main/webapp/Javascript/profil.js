4750bbb81c992a265103a2eef9b9523c809b6f9f
hallvsa <hallvsa@stud.ntnu.no>
2018-01-23 14:40:53 +0100

    Fikset profil, og oppdaterte controller klasse

--- src/main/webapp/Javascript/profil.js
+++ src/main/webapp/Javascript/profil.js
@@ -449,23 +449,6 @@ function hentliste() {
             '<div id="list1" class="list-group"></div></div></div>');
 
 
-        //Gammel design, kan trykkes på
-        /*$("#husstander").append('<div class="panel panel-default"><div class="panel-heading clearfix" data-toggle="collapse" data-parent="#husstander"' +
-            ' data-target="#' + husholdningId + '" onclick="displayDiv()"><h4 class= "panel-title col-md-9"><a></a>' + husholdnavn + '</h4>' +
-            '<div class = "collapse"><button id="meldut"' +
-            ' class="btn btn-danger pull-right removeButton col-md-3" type="button">Forlat</button></div>' +
-            '</div><div id="' + husholdningId + '"' +
-            ' class="panel-collapse collapse invisibleDiv"><div class="panel-body"><ul class="list-group" id="hhliste'+husholdningId+'"></ul>' +
-            '<div id="list1" class="list-group">' + '</div></div></div></div>');*/
-
-        /* $("#accordion").append('<li class="panel panel-default">' +
-         '<div class="panel-heading clearfix"><h4 class="panel-title pull-left" style="padding-top: 7.5px;">' +
-         ' <a data-toggle="collapse" data-parent="#accordion" href="#collapse1">'+husholdnavn+'</a> </h4> <div>' +
-         '<button id="slett '+ husholdnavn+'" type="button" class="btn btn-danger pull-right">Slett husstand</button></div></div> ' +
-         '<div id="collapse1" class="panel-collapse collapse in"> <div class="panel-body">' +
-         '<ul class="list-group" id='+husholdnavn+'>');*/
-
-
         for (var p = 0, lengt2 = mineHusholdninger[k].medlemmer.length; p < lengt2; p++) {
             var medlemnavn = mineHusholdninger[k].medlemmer[p].navn;
             console.log(medlemnavn);
