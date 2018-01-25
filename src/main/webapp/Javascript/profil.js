9e6c3e9b5a20952887669c3e8220f87f0ea4f3da
noraor <noraor@stud.ntnu.no>
2018-01-25 13:34:34 +0100

    fiksa profil igjen

--- src/main/webapp/Javascript/profil.js
+++ src/main/webapp/Javascript/profil.js
@@ -256,7 +256,7 @@ function hentliste() {
         $("#husstander").append('<div  class="panel panel-default container-fluid"><div class="panel-heading clearfix row" ' +
             'data-toggle="collapse" data-parent="#husstander"' +
             ' data-target="#' + husholdningId + '" onclick="displayDiv()">' +
-            '<h4 class= "col-md-9 panel-title" style="display: inline; padding: 0px">' + husholdnavn + '</h4>' +
+            '<h4 class= "col-md-9 panel-title">' + husholdnavn + '</h4>' +
                 '<div class="stjerneogforlat pull-right">' +
             '<span id="star'+husholdningId+'" value="'+husholdningId+'" style="font-size: 1.7em;' +
             ' color: orange" role="button" class="glyphicon '+string+'"></span>' + " " +
