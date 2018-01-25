bf54aa3013acf58829f7ec4c86ff9a2ed7440f4e
kimiaa <kimiaa@stud.ntnu.no>
2018-01-25 12:39:32 +0100

    små endringer rundt omkring

--- src/main/java/server/controllers/BrukerController.java
+++ src/main/java/server/controllers/BrukerController.java
@@ -9,6 +9,7 @@ import server.restklasser.*;
 import server.util.Encryption;
 import server.util.RandomGenerator;
 
+import java.security.SecureRandom;
 import java.sql.*;
 import java.util.Objects;
 
@@ -20,9 +21,13 @@ public class BrukerController {
     private static PreparedStatement ps;
     private static Statement s;
     private final static String TABELLNAVN = "bruker";
+    private final static int PASSWORD_LENGTH = 8;
+    private static final SecureRandom RANDOM = new SecureRandom();
+
 
     /**
      * Henter navn på bruker gitt brukerens id.
+     *
      * @param brukerid int id som identifiserer en bruker.
      * @return String navnet til brukeren.
      */
@@ -33,6 +38,7 @@ public class BrukerController {
 
     /**
      * Henter epost-adressen til en bruker gitt brukerens id.
+     *
      * @param brukerid int id som identifiserer en bruker.
      * @return String epost-adressen.
      */
@@ -42,12 +48,14 @@ public class BrukerController {
 
     /**
      * Henter favoritthusholdning til bruker
+     *
      * @param brukerid til brukeren vi finner favoritthusholdningen til
      * @return id til favoritthusholdning
      */
     public static String getFavoritthusholdning(int brukerid) {
         return GenereltController.getString("favorittHusholdning", TABELLNAVN, brukerid);
     }
+
     public static int getBrukerId(String epost) {
         return GenereltController.getInt("brukerId", TABELLNAVN, "epost", epost);
     }
@@ -60,17 +68,18 @@ public class BrukerController {
 
     /**
      * Sletter et medlem fra en husholdning gitt brukerens id.
+     *
      * @param brukerid int id som identifiserer en bruker
      * @return true om brukeren ble slettet, false om noe gikk galt under sletting.
      */
     public static boolean slettFraHusholdning(int brukerid, int husholdningid) {
         String getQuery = "DELETE FROM hhmedlem WHERE brukerId = " + brukerid + " AND husholdningId =" + husholdningid;
 
-        try (Connection con = ConnectionPool.getConnection()){
+        try (Connection con = ConnectionPool.getConnection()) {
             ps = con.prepareStatement(getQuery);
             ps.executeUpdate();
             return true;
-        }catch (SQLException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
             return false;
         }
@@ -78,6 +87,7 @@ public class BrukerController {
 
     /**
      * Registrerer en bruker i systemet.
+     *
      * @param bruker er et Bruker-objekt som skal registreres
      * @return true dersom bruker ble registrert, false om noe gikk galt under registrering.
      */
@@ -91,7 +101,7 @@ public class BrukerController {
         String query = "INSERT INTO bruker (hash, navn, epost, salt) VALUES (?, ?, ?, ?)";
 
 
-        try (Connection con = ConnectionPool.getConnection()){
+        try (Connection con = ConnectionPool.getConnection()) {
 
             ps = con.prepareStatement(epostLedig);
             ps.setString(1, epost);
@@ -118,6 +128,7 @@ public class BrukerController {
 
     /**
      * Sjekker om epost og passord stemmer.
+     *
      * @param bruker brukeren som sjekkes
      * @return brukerdata hvis ok: epost, navn, id, favoritthusholdning, gjøremal
      */
@@ -128,14 +139,15 @@ public class BrukerController {
             ps.setString(1, bruker.getEpost());
             try (ResultSet rs = ps.executeQuery()) {
                 rs.next();
-                if (!Encryption.instance.isPassOk(bruker.getPassord(), rs.getString("hash"), rs.getString("salt"))) return null;
+                if (!Encryption.instance.isPassOk(bruker.getPassord(), rs.getString("hash"), rs.getString("salt")))
+                    return null;
                 bruker.setNavn(rs.getString("navn"));
                 bruker.setBrukerId(rs.getInt("brukerId"));
                 bruker.setFavHusholdning(rs.getInt("favorittHusholdning"));
                 String hentGjoremal = "SELECT * FROM gjoremal WHERE utførerId = " + bruker.getBrukerId() + " AND fullført = 0";
                 PreparedStatement psGjoremal = con.prepareStatement(hentGjoremal);
                 ResultSet rs2 = psGjoremal.executeQuery();
-                while(rs2.next()){
+                while (rs2.next()) {
                     Gjoremal gjoremal = new Gjoremal();
                     gjoremal.setFrist(rs2.getDate("frist"));
                     gjoremal.setHusholdningId(rs2.getInt("husholdningId"));
@@ -160,11 +172,12 @@ public class BrukerController {
 
     /**
      * Setter ny favoritthusholdning til brukeren
-     * @param brukerId til brukeren det gjelder.
+     *
+     * @param brukerId      til brukeren det gjelder.
      * @param husholdningId til husholdningen som skal bli favoritt
      */
     public static boolean setNyFavoritthusholdning(int brukerId, String husholdningId) {
-         return GenereltController.update(TABELLNAVN, "favorittHusholdning", husholdningId, brukerId);
+        return GenereltController.update(TABELLNAVN, "favorittHusholdning", husholdningId, brukerId);
     }
 
     public static void setNyEpost(String epost, int brukerId) {
@@ -173,25 +186,43 @@ public class BrukerController {
 
     /**
      * Lager nytt passord og lagrer det i databasen
+     *
      * @param brukerId til personen som trenger nytt passord
      * @return det nye passordet
      */
     public static String nyttTilfeldigPass(int brukerId) {
-        String passord = RandomGenerator.stringulns(8);
-        setNyttPassord(brukerId, passord);
-        return passord;
+        if (PASSWORD_LENGTH < 1) {
+            return "The length of the password generated must be positive";
+        }
+        StringBuilder sb = new StringBuilder(PASSWORD_LENGTH);
+        for (int i = 0; i < PASSWORD_LENGTH; i++) {
+            int c = RANDOM.nextInt(62);   // generates a random int from zero to 62
+            if (c <= 9) {
+                sb.append(String.valueOf(c));   // adds the number to the StringBuilder
+            } else if (c < 36) {
+                sb.append((char) ('a' + c - 10));   // adds the lower case letter of the number to the StringBuilder
+            } else {
+                sb.append((char) ('A' + c - 36));   // adds the upper case letter of the number to the StringBuilder
+            }
+        }
+        setNyttPassord(brukerId, sb.toString());
+        return sb.toString();
     }
+        /*String passord = RandomGenerator.stringulns(8);
+        setNyttPassord(brukerId, passord);
+        return passord;*/
 
     /**
      * Oppdaterer databasen med den nye hashen og saltet
+     *
      * @param brukerId til brukeren som får nytt passord
-     * @param passord som skal hashes
+     * @param passord  som skal hashes
      */
     public static void setNyttPassord(int brukerId, String passord) {
         String[] hashOgSalt = Encryption.instance.passEncoding(passord);
         String sqlSetning = "update " + TABELLNAVN + " set hash=?, salt=? where " + TABELLNAVN + "id=" + brukerId;
-        try(Connection connection = ConnectionPool.getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement(sqlSetning)) {
+        try (Connection connection = ConnectionPool.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sqlSetning)) {
             preparedStatement.setString(1, hashOgSalt[0]);
             preparedStatement.setString(2, hashOgSalt[1]);
             preparedStatement.executeUpdate();
@@ -200,7 +231,7 @@ public class BrukerController {
         }
     }
 
-    public static void setNyttNavn(int brukerId, String navn){
+    public static void setNyttNavn(int brukerId, String navn) {
         GenereltController.update(TABELLNAVN, "navn", navn, brukerId);
     }
 
