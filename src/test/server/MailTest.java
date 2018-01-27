66db9528f5290ead77d61fdc2d7a81e08d3b8e49
trulsmt <trulsmt@stud.ntnu.no>
2018-01-27 11:48:25 +0100

    lagt til at man får generert passord på mail hvis man blir lagt til i en husstand uten å være bruker fra før av.

--- src/test/server/MailTest.java
+++ src/test/server/MailTest.java
@@ -2,6 +2,7 @@ package server;
 
 import org.junit.Test;
 import server.Mail;
+import server.restklasser.Bruker;
 
 import java.util.ArrayList;
 
@@ -16,8 +17,24 @@ public class MailTest {
         ArrayList<String> eposter = new ArrayList<>();
         eposter.add("kimia.abtahi@gmail.com");
         //eposter.add("trulsmatias@gmail.com");
-        //Mail.sendAllerede(eposter, "Vår hus");
+        //Mail.sendAllerede(eposter, "Vårt hus");
     }
+
+    @Test
+    public void sendUreg() throws Exception {
+        ArrayList<Bruker> brukerArrayList = new ArrayList<>();
+        Bruker bruker1 = new Bruker();
+        Bruker bruker2 = new Bruker();
+        bruker1.setEpost("trulsmatias@gmail.com");
+        bruker2.setEpost("trulsmatias@icloud.com");
+        bruker1.setPassord("passord123");
+        bruker2.setPassord("passord123");
+        brukerArrayList.add(bruker1);
+        brukerArrayList.add(bruker2);
+        Mail.sendUreg(brukerArrayList, "tha Crib");
+    }
+
+
 
     /*@Test
     public void sendGlemtPassord() throws Exception {
