a26fed919ba92ee47dd751b3d4aacf75f3e9e002
tonivuc <tonivuc@gmail.com>
2018-01-28 18:43:50 +0100

    La til variabler for setup av databasen

--- src/main/java/server/database/ConnectionPool.java
+++ src/main/java/server/database/ConnectionPool.java
@@ -26,7 +26,7 @@ public final class ConnectionPool {
 
     private ConnectionPool() {
         dataSource.setTimeBetweenEvictionRunsMillis(10000); //10 minutter mellom hver gang den sjekker for idle connections
-        dataSource.setMaxTotal(20);
+        dataSource.setMaxTotal(20); //Tror ikke skolens database lar oss ha mer.
     }
 
     //Lager en connection mot databasen hvis en connection ikke finnes. Finnes en connection tar den
